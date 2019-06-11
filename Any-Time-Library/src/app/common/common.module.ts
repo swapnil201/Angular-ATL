32920bc844fec29456496dbaea7ef0b183745658
Swapnil Nayak <M1041656@mindtree.com>
2019-06-11 22:50:05 +0530

    initial layout

--- Any-Time-Library/src/app/common/common.module.ts
+++ Any-Time-Library/src/app/common/common.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from "@angular/material/icon";
 
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
-  declarations: [FooterComponent, HeaderComponent],
+  declarations: [FooterComponent, HeaderComponent, LayoutComponent],
   imports: [
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatIconModule
   ],
   exports: [
-    HeaderComponent
+    LayoutComponent
   ]
 })
 export class CommonModule { }
