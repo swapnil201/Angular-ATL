f3b0f4a2efe912cdd08199118cfbe7ddd72dc0f5
Swapnil <nayakswapnil00@yahoo.com>
2019-07-08 09:10:54 +0530

    book model

---
+++ Any-Time-Library/src/app/books/book.model.ts
@@ -0,0 +1,10 @@
+export class Book {
+    public title: string;
+    public description: string;
+    public author: string;
+    public imgPath: string;
+    public rating: number;
+    public totalCopies: number;
+    public borrowedCopies: number;
+    public timesBorrowed: number;
+}
